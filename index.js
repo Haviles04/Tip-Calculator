997c30e05e8f0a9fe7e22f8b87e02fd0f886c78c
Haviles04 <Haviles04@gmail.com>
2022-09-25 15:46:39 -0400

    reverted a little bit so the JS works. still not 100%

--- index.js
+++ index.js
@@ -16,64 +16,44 @@ const customTipClear = () => {
   if (custom.value != "") custom.value = "";
 };
 
-// Calculates everything
+// Calculation function 
 const sumItUp = () => {
-
-//Checks to see if the tip button is clicked, if so saves the value as the tipBTNValue, if not sets it as undefined
-
-
-let tipBtnVal = 0
-
-const isTipBtnClick = () =>{
-    const clickedTip = document.querySelectorAll(".tipBtn")
-    for (let i = 0; i < clickedTip.length; i++){
-      if(clickedTip[i].checked = true){
-        let tipBtnVal = clickedTip[i].value;
-      }else{
-        return tipBtnVal = 0;
-      }
-    }
-  }
-
-
-
+  
   const customVal = document.getElementById("customTip").value;
   const billAmount = document.getElementById("billAmount").value;
   const numOfPeople = document.getElementById("peopleAmount").value;
-  const tipAmountDisplay = document.getElementById("tipAmountBig");
-  const totalAmountDisply = document.getElementById("totalAmountBig");
-  const peopleError = document.getElementsByClassName("invalidPeople");
-  const tipError = document.getElementsByClassName("invalidTip");
-  const billError = document.getElementsByClassName("invalidBill");
+  const tipBtnVal = document.querySelector(".tipBtn:checked").value;
 
- 
  // checks to make sure each field is filled out, if not displays an error message. if everything is good runs the function sum it up;
- 
+  
+  /* Doesn't do anything (not sure why)
   if (numOfPeople === 0){
-    peopleError.style.visibilty = "visible";
+    document.getElementsByClassName("invalidPeople").style.visibilty = "visible";
   }else if(billAmount === 0){
-    billError.style.visibilty = "visible";
+    document.getElementsByClassName("invalidBill").style.visibilty = "visible";
   }else if(tipBtnVal === 0 && customVal === 0){
-    tipError.style.visibility = "visible";
+    document.getElementsByClassName("invalidTip").style.visibility = "visible";
   }else {
+  */
 
 
     // calculates all numbers needed
     let tipAmt = 0;
-    if (tipBtnVal === undefined) {
-      tipAmt = billAmount * (customVal / 100);
+    if (tipBtnVal === 0) {
+      tipAmt = billAmount * (parseInt(customVal) / 100);
     } else {
       tipAmt = billAmount * tipBtnVal;
     }
 
     let total = (parseInt(billAmount) + parseInt(tipAmt)) / numOfPeople;
-
+    
+    //Rounds number to 2 decimal places
     tipAmountFinal = tipAmt.toFixed(2);
     totalFinal = total.toFixed(2);
 
     // Displays numbers to user
-    tipAmountDisplay.innerHTML = `$ ${tipAmountFinal}`;
-    totalAmountDisply.innerHTML = `$ ${totalFinal}`;
+    document.getElementById("tipAmountBig").innerHTML = `$ ${tipAmountFinal}`;
+    document.getElementById("totalAmountBig").innerHTML = `$ ${totalFinal}`;
   }
 };
 
