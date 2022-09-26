532b052db195a91a5b43f82462bad30e9123122c
Haviles04 <Haviles04@gmail.com>
2022-09-26 19:41:46 -0400

    changed javascrip to run a switch for error labels

--- index.js
+++ index.js
@@ -18,47 +18,93 @@ const customTipClear = () => {
 
 // Calculation function
 const sumItUp = () => {
-  const customVal = document.getElementById("customTip").value;
-  const billAmount = document.getElementById("billAmount").value;
-  const numOfPeople = document.getElementById("peopleAmount").value;
+  
+  const customVal = Number(document.getElementById("customTip").value);
+  const billAmount = Number(document.getElementById("billAmount").value);
+  const numOfPeople = Number(document.getElementById("peopleAmount").value);
 
   const getTipValue = () => {
     const tipBtns = document.querySelectorAll(".tipBtn:checked");
-    return tipBtns.length ? tipBtns[0].value : 0;
+    return tipBtns.length ? Number(tipBtns[0].value) : 0;
   };
-
   const tipBtnVal = getTipValue();
 
   // checks to make sure each field is filled out, if not displays an error message. if everything is good runs the function sum it up;
   // doesnt work yet (don't know why)
 
-  if (numOfPeople === false) {
-    document.getElementsByClassName("invalidPeople").style.visibilty =
-      "visible";
-  } else if (billAmount === false) {
-    document.getElementsByClassName("invalidBill").style.visibilty = "visible";
-  } else if (tipBtnVal === false && customVal === false) {
-    document.getElementsByClassName("invalidTip").style.visibility = "visible";
-  } else {
-    // calculates all numbers needed
-    let tipAmt = 0;
-    if (tipBtnVal === 0) {
-      tipAmt = billAmount * (parseInt(customVal) / 100);
-    } else {
-      tipAmt = billAmount * tipBtnVal;
-    }
+  let error = ((numOfPeople === 0) || (billAmount === 0) || (tipBtnVal === 0 && customVal === 0) || (numOfPeople > 0) || (billAmount > 0) || (tipBtnVal > 0 && customVal === 0) || (tipBtnVal === 0 && customVal > 0));
 
-    let total = (parseInt(billAmount) + parseInt(tipAmt)) / numOfPeople;
+  switch (error){
+    case (billAmount > 0 && (tipBtnVal === 0 && customVal === 0)&& numOfPeople >0):
+      document.getElementById('invalidBill').style.visibility="hidden";
+      document.getElementById('invalidTip').style.visibility="visible";
+      document.getElementById('invalidPeople').style.visibility="hidden";
+    sumItUp();
+    case (billAmount > 0 && (tipBtnVal === 0 && customVal === 0)&& numOfPeople === 0):
+      document.getElementById('invalidBill').style.visibility="hidden";
+      document.getElementById('invalidTip').style.visibility="visible";
+      document.getElementById('invalidPeople').style.visibility="visible";     
+    sumItUp();
+    case (billAmount > 0 && ((tipBtnVal > 0 && customVal === 0) || (tipBtnVal === 0 && customVal > 0)) && numOfPeople === 0):
+      document.getElementById('invalidBill').style.visibility="hidden";
+      document.getElementById('invalidTip').style.visibility="hidden";
+      document.getElementById('invalidPeople').style.visibility="visible";  
+    sumItUp();
+    case (billAmount === 0 && ((tipBtnVal > 0 && customVal === 0) && (tipBtnVal === 0 && customVal > 0)) && numOfPeople === 0):
+      document.getElementById('invalidBill').style.visibility="visible";
+      document.getElementById('invalidTip').style.visibility="hidden";
+      document.getElementById('invalidPeople').style.visibility="visible";  
+    sumItUp();
+    case (billAmount === 0 && ((tipBtnVal > 0 && customVal === 0) || (tipBtnVal === 0 && customVal > 0)) && numOfPeople > 0):
+      document.getElementById('invalidBill').style.visibility="visible";
+      document.getElementById('invalidTip').style.visibility="hidden";
+      document.getElementById('invalidPeople').style.visibility="hidden";  
+    sumItUp();
+    case (billAmount === 0 && (tipBtnVal === 0 && customVal === 0)&& numOfPeople >0):
+      document.getElementById('invalidBill').style.visibility="visible";
+      document.getElementById('invalidTip').style.visibility="visible";
+      document.getElementById('invalidPeople').style.visibility="hidden";  
+    sumItUp();  
+  
+    case (billAmount > 0 && ((tipBtnVal > 0 && customVal === 0) || (tipBtnVal === 0 && customVal > 0)) && numOfPeople > 0 ):
+      document.getElementById('invalidBill').style.visibility="hidden";
+      document.getElementById('invalidTip').style.visibility="hidden";
+      document.getElementById('invalidPeople').style.visibility="hidden"; 
+      document.querySelector('.resetBtn').classList.add="resetOn"; 
 
-    //Rounds number to 2 decimal places
-    tipAmountFinal = tipAmt.toFixed(2);
-    totalFinal = total.toFixed(2);
+
+
+        // calculates all numbers needed
+        
+        let tipAmt = 0;
+        if (tipBtnVal === 0) {
+          tipAmt = (billAmount * (customVal / 100)) / numOfPeople;
+        } else {
+          tipAmt = (billAmount * tipBtnVal) / numOfPeople;
+        }
+
+        let total = (billAmount + (tipAmt * numOfPeople)) / numOfPeople;
+
+        //Rounds number to 2 decimal places
+        tipAmountFinal = tipAmt.toFixed(2);
+        totalFinal = total.toFixed(2);
+  }
 
     // Displays numbers to user
+
     document.getElementById("tipAmountBig").innerHTML = `$ ${tipAmountFinal}`;
     document.getElementById("totalAmountBig").innerHTML = `$ ${totalFinal}`;
-  }
+    
+  
+
 };
+
+const resetAll = () => {
+  document.getElementById("optionsForm").reset();
+  document.getElementById("tipAmountBig").innerHTML = "$ 0.00";
+  document.getElementById("totalAmountBig").innerHTML = "$ 0.00";
+}
+
 
 // calls for sumItUp function to run whenever anything on any form is changed
 const billAmountInput = document
@@ -70,6 +116,7 @@ const numOfPeopleInput = document
 const customTipInput = document
   .getElementById("customTip")
   .addEventListener("input", sumItUp);
+const resetBtnInput = document.getElementById("reset").addEventListener('click', resetAll)
 
 /* need to get function sumItUp to run on radio button click....
 
