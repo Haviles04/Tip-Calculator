d072034788b44c22dffd9effc52cc85415a5f56d
Haviles04 <Haviles04@gmail.com>
2022-09-25 21:02:55 -0400

    changed JS function to find tip btn value, added SumItUp function to onclick in radio button HTML

--- index.js
+++ index.js
@@ -16,27 +16,30 @@ const customTipClear = () => {
   if (custom.value != "") custom.value = "";
 };
 
-// Calculation function 
+// Calculation function
 const sumItUp = () => {
-  
   const customVal = document.getElementById("customTip").value;
   const billAmount = document.getElementById("billAmount").value;
   const numOfPeople = document.getElementById("peopleAmount").value;
-  const tipBtnVal = document.querySelector(".tipBtn:checked").value;
 
- // checks to make sure each field is filled out, if not displays an error message. if everything is good runs the function sum it up;
-  
-  /* Doesn't do anything (not sure why)
-  if (numOfPeople === 0){
-    document.getElementsByClassName("invalidPeople").style.visibilty = "visible";
-  }else if(billAmount === 0){
+  const getTipValue = () => {
+    const tipBtns = document.querySelectorAll(".tipBtn:checked");
+    return tipBtns.length ? tipBtns[0].value : 0;
+  };
+
+  const tipBtnVal = getTipValue();
+
+  // checks to make sure each field is filled out, if not displays an error message. if everything is good runs the function sum it up;
+  // doesnt work yet (don't know why)
+
+  if (numOfPeople === false) {
+    document.getElementsByClassName("invalidPeople").style.visibilty =
+      "visible";
+  } else if (billAmount === false) {
     document.getElementsByClassName("invalidBill").style.visibilty = "visible";
-  }else if(tipBtnVal === 0 && customVal === 0){
+  } else if (tipBtnVal === false && customVal === false) {
     document.getElementsByClassName("invalidTip").style.visibility = "visible";
-  }else {
-  */
-
-
+  } else {
     // calculates all numbers needed
     let tipAmt = 0;
     if (tipBtnVal === 0) {
@@ -46,7 +49,7 @@ const sumItUp = () => {
     }
 
     let total = (parseInt(billAmount) + parseInt(tipAmt)) / numOfPeople;
-    
+
     //Rounds number to 2 decimal places
     tipAmountFinal = tipAmt.toFixed(2);
     totalFinal = total.toFixed(2);
@@ -55,7 +58,7 @@ const sumItUp = () => {
     document.getElementById("tipAmountBig").innerHTML = `$ ${tipAmountFinal}`;
     document.getElementById("totalAmountBig").innerHTML = `$ ${totalFinal}`;
   }
-;
+};
 
 // calls for sumItUp function to run whenever anything on any form is changed
 const billAmountInput = document
@@ -68,7 +71,7 @@ const customTipInput = document
   .getElementById("customTip")
   .addEventListener("input", sumItUp);
 
-/* need to get function sumItUp to run on button click....
+/* need to get function sumItUp to run on radio button click....
 
 
 make if then statement on num of people / bill amount to make error message so up if not filled...
